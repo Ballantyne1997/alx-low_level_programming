4a8fd15668b8caefbae065cc32b617cb4d24cadf
Ballantyne1997 <thobaniballantyne@icloud.com>
2023-06-15 16:27:34 -0700

    name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+/**
+ * main - A program that prints a line with put function
+ * Return: 0 (Success)
+ */
+int main(voiid)
+{
+puts ("\"Programming is the line missing a  multillingual puzzLE);
+return (0);
+}
