0d2bd9fc24b372b5adc7216ff482dfcf54ecd333
Ballantyne1997 <thobaniballantyne@icloud.com>
2023-06-22 18:25:01 -0700

    Most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
  * Return: void
  */
 
-void print_mos_numbers(void)
+void print_most_numbers(void)
 {
-	char c;
+char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
 	_putchar('\n');
 }
