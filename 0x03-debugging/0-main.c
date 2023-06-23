63e37eb4e084a9d70efc58295593341b02088529
Ballantyne1997 <thobaniballantyne@icloud.com>
2023-06-22 20:01:14 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+
+/**
+ * main - tests function that prints if an integer is ngative or positive
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
