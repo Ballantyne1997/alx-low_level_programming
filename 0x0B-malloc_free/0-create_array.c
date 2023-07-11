0efef95f708736ad2efa1c25946384e15c6063ad
Ballantyne1997 <thobaniballantyne@icloud.com>
2023-07-11 13:09:51 -0700

    Float like a butterfly

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  * @size: The size of the array
  * @c: The char to fill in the array
  *
- * Return: The array filled 
+ * Return: The array filled
  */
 
 char *create_array(unsigned int size, char c)
